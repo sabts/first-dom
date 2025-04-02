0588db20527a1f67eeadb94697b4e6d45d614994
sabts <sabrinataraschi@gmail.com>
2025-04-03 00:01:26 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,11 @@ console.log(
 const elementBox3 = document.getElementById("subtitle-1");
 console.dir(elementBox3)
 
-console.log(`${elementBox3.children[0].textContent}`) 
+console.dir(`Text content del h2: ${elementBox3.parentElement.textContent} Clase del div: ${elementBox3.parentElement.classList}`) 
+console.dir(`Text content de p:${elementBox3.nextElementSibling.textContent} Clase del div: ${elementBox3.nextElementSibling.classList}`) 
 
 //Del h2 con el id subtitle-2 imprime por consola: Su textContent; el textContent y la clase de los dos span que tiene.
+const elementSubtitle2 = document.getElementById("subtitle-2")
+
+console.dir(`Text content del h2: ${elementSubtitle2.textContent}`)
+console.dir(`Text content del span: ${elementSubtitle2.previousElementSibling.textContent}`)
